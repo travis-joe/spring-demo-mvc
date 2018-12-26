f9be0b95966931081ac4c26eb4be5af88705d178
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-12-26 14:14:42 +0800

    123 solve ambiguous mapping

---
+++ src/com/travis/springdemo/mvc/SillyController.java
@@ -0,0 +1,15 @@
+package com.travis.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+
+public class SillyController {
+
+    @RequestMapping("/showForm")
+    public String displayForm(){
+        return "silly";
+    }
+}
